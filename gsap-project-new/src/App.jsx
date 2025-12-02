ff35b924a347716bc7c70efdee0d056bbf1052fb
moawedmohmaed <moawedmohmaed672@gmail.com>
2025-12-02 21:45:54 +0200

    implement the first part of the hero section

---
+++ gsap-project-new/src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./component/Hero";
+import Navbar from "./component/Navbar";
+
+function App() {
+  return (
+    <main>
+      <Navbar />
+      <Hero  />
+    </main>
+  );
+}
+
+export default App;
